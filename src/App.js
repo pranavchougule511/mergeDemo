c9f127140d8716aa8872137318bacc2652c4519e
pranavchougule511 <pranavchougule10@gmail.com>
2025-09-01 19:06:49 +0530

    Add Learn Git info

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn GIT with me...
         </a>
       </header>
     </div>
